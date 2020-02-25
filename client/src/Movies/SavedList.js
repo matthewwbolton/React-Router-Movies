36dc3bc3d3e58be5bab0a3c444329e12acb5bc9e
Matthew Bolton <53613881+matthewwbolton@users.noreply.github.com>
2020-02-25 15:58:50 -0500

    refactored code to use render props and component props to set up SPA

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
+      <Link to={`/movies/${movie.id}`} key={movie.id}>
       <span className="saved-movie">{movie.title}</span>
+      </Link>
     ))}
-    <div className="home-button">Home</div>
+    <Link to='/'>
+      <div className="home-button">Home</div>
+    </Link>
   </div>
 );
 
